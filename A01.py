33d82f45e5c4bb862e061114f70c2514b5296e33
Matthew <townsemc@sunypoly.edu>
2024-09-13 16:19:02 -0700

    A01 pretest and E02

---
+++ A01.py
@@ -0,0 +1,71 @@
+import cv2
+import shutil
+import os
+import sys
+from pathlib import Path
+
+def load_video_as_frames(video_filePath):
+    capture = cv2.VideoCapture(video_filePath)
+    if capture.isOpened():
+        print("ERROR: Could not open or find the video!")
+        exit(1)
+    frames=[]
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        frames.append(frame)
+    capture.release()
+    return frames
+
+def compute_wait(fps):
+    return int(1000.0/fps)
+    
+
+def display_frames(all_frames, title, fps=30):
+    wait = compute_wait(fps)
+    for frame in all_frames:
+        cv2.imshow(title, frame)
+        if cv2.waitKey(wait):
+            break
+
+    cv2.destroyAllWindows()
+    
+def save_frames(all_frames, output_dir, basename, fps=30):
+    video_folder = f"{basename}_{fps}"
+    outputPath = os.path.join(output_dir, video_folder)
+    
+    if os.path.exists(outputPath):
+        shutil.rmtree(outputPath)
+    
+    os.makedirs(outputPath)
+    for index, frame in enumerate(all_frames):
+        filename = "image_%07d.png" % index
+        full_path = os.path.join(outputPath, filename)
+        cv2.imwrite(full_path, frame)
+        
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python script.py <video_filepath> <output_directory>")
+        sys.exit(1)
+    
+    video_filepath = sys.argv[1]
+    output_dir = sys.argv[2]
+    
+    # Get the core filename from the video path
+    basename = Path(video_filepath).stem
+    
+    # Load frames
+    all_frames = load_video_as_frames(video_filepath)
+    if all_frames is None:
+        print("Error: Failed to load frames.")
+        sys.exit(1)
+    
+    # Display frames
+    display_frames(all_frames, "Input Video", fps=30)
+    
+    # Save frames
+    save_frames(all_frames, output_dir, basename, fps=30)
+
+if __name__ == "__main__":
+    main()
--- Exercisies02.py
+++ Exercisies02.py
@@ -27,11 +27,67 @@ import cv2
 import pandas
 import sklearn
 
+def filterNeighborhood2D(image, kernal, crow, ccol):
+    halfH = kernal.shape[0]//2
+    halfW = kernal.shape[1]//2
+    
+    startOFFH = (1-kernal.shape[0]%2)
+    startOFFW = (1 - kernal.shape[1]%2)
+    
+    endRow = crow + halfH
+    endCol = ccol + halfW
+    
+    startRow = crow - halfH + startOFFH
+    startCol = ccol = halfW + startOFFW
+    
+    
+    clamp_startRow = max(0, startRow)
+    clamp_startCol = max(0, startCol)
+    neighborhood = image[clamp_startRow:(endRow+1), clamp_startCol:(endCol+1)]
+    
+    if startRow < 0:
+        kernal = kernal[-startRow:]
+    elif endRow > (image.shape[0]-1):
+        off = image.shape[0] -1 - endRow
+        kernal = kernal[0:(kernal.shape[0]-off)]
+    if startCol < 0:
+        kernal = kernal[:, -startCol:]
+    elif endCol > (image.shape[1]-1):
+        off = image.shape[1] - 1 - endCol
+        kernal = kernal[:, 0:(kernal.shape[1]+off)]
+    
+    print("NEIGHBORHOOD: ", neighborhood.shape)
+    print("KERNAL:", kernal.shape)
+    
+    value = kernal * neighborhood
+    value = np.sum(value)
+    
+    return value
+
+def filter2D(image, kernal):
+    output = np.copy(image)
+    
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+            output[row,col] = filterNeighborhood2D(image, kernal, row, col)
+    return output
+
+def filter(video, kernal):
+    output = np.copy(video)
+    
+    for t in range(video.shape[0]):
+        for row in range(video.shape[1]):
+            for col in range(video.shape[2]):
+
 ###############################################################################
 # MAIN
 ###############################################################################
 
 def main():        
+    dummy_image = np.zeros((10,10), dtype="float64")
+    dummy_filter= np.zeros((3,3), dtype="float64")
+    dummy_output = filter2D(dummy_image, dummy_filter)
+    
     ###############################################################################
     # PYTORCH
     ###############################################################################
@@ -58,7 +114,7 @@ def main():
         print("Opening webcam...")
 
         # Linux/Mac (or native Windows) with direct webcam connection
-        capture = cv2.VideoCapture(1) #, cv2.CAP_DSHOW) # CAP_DSHOW recommended on Windows 
+        capture = cv2.VideoCapture(0) #, cv2.CAP_DSHOW) # CAP_DSHOW recommended on Windows 
         # WSL: Use Yawcam to stream webcam on webserver
         # https://www.yawcam.com/download.php
         # Get local IP address and replace
@@ -118,7 +174,8 @@ def main():
             
             cv2.imshow("GRAY", gray_image)
             
-            fx = cv2.filter2D(gray_image, cv2.CV_64F, kfx)
+            #fx = cv2.filter2D(gray_image, cv2.CV_64F, kfx)
+            
             cv2.imshow("FX", np.absolute(fx)*4.0)
             
             if prev_frame is None:
